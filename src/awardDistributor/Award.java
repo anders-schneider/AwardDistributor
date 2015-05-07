809aa08dd7fad0542cb22a9ba2ad00cc9e7d8566
anders-schneider <aschn@seas.upenn.edu>
2015-05-07 00:19:52 -0400

    Added ranking matrix initialization method

--- src/awardDistributor/Award.java
+++ src/awardDistributor/Award.java
@@ -11,12 +11,59 @@ package awardDistributor;
 public class Award {
 	
 	private String name;
+	private int index;
+	private String[] noms;
 	
+
+	private static int numAwards = 0;
+	
+	/**
+	 * Constructor of the Award class, takes a String representing the name
+	 * of the Award
+	 * @param name
+	 */
+	public Award(String name) {
+		this.name = name;
+		index = numAwards;
+		numAwards++;
+	}
+	
+	/**
+	 * Returns the index of this Award: its ordinal position relative to
+	 * all the other awards that have been created.
+	 * 
+	 * @return This Award's index
+	 */
+	public int getIndex() {
+		return index;
+	}
+	
+	/**
+	 * Returns the array of nominees for this Award.
+	 * 
+	 * @return The array of nominees for this Award
+	 */
+	public String[] getNoms() {
+		return noms;
+	}
+	
+	/**
+	 * Returns the hashcode corresponding to this Award.
+	 * 
+	 * @return The hashcode corresponding to this Award.
+	 */
 	@Override
 	public int hashCode() {
 		return name.hashCode();
 	}
 
+	/**
+	 * Compares this Award with the input object for equality. Two Awards
+	 * are equal if they share the same name.
+	 * 
+	 * @param o The object to which this Award is being compared
+	 * @return True if the Awards have the same name, false otherwise
+	 */
 	@Override
 	public boolean equals(Object o) {
 		if (!(o instanceof Award)) {
--- src/awardDistributor/AwardDistributor.java
+++ src/awardDistributor/AwardDistributor.java
@@ -1,5 +1,8 @@
 package awardDistributor;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * The AwardDistributor class determines the optimal distribution of a number
  * of awards based on ranked lists of nominees for each award. Nominees may be
@@ -15,9 +18,63 @@ package awardDistributor;
 
 public class AwardDistributor {
 	
-	private Award[] awardList;
+	private int nomsPerAward;
+	
+	/**
+	 * Constructor that takes the number of nominations per award as input.
+	 * 
+	 * @param nomsPerAward The number of nominations per award
+	 */
+	public AwardDistributor(int nomsPerAward) {
+		this.nomsPerAward = nomsPerAward;
+	}
+	
+	/**
+	 * Returns a HashMap with the optimal distribution of awards (keys) amongst
+	 * the nominees (values).
+	 * 
+	 * @param awards An array of all awards
+	 * @param noms An array of all nominees across all awards
+	 * @return A HashMap with the optimal award-nominee pairs
+	 */
+	public HashMap<Award, String> findOptimalDistribution(Award[] awards, 
+														  ArrayList<String> noms) {
+		if (awards.length > noms.size()) throw new IllegalArgumentException("More awards than nominees");
+		
+		int[][] rankingMatrix = generateRankingMatrix(awards, noms);
+		
+		return null;
+	}
 
-	public AwardDistributor(Award[] awardList) {
-		this.awardList = awardList;
+	/**
+	 * Returns a 2D array of integers. The value in location (i, j) represents 
+	 * how highly ranked Nominee j is for Award i. The best possible ranking is
+	 * a 1, all other rankings are larger integers. If a nominee is not ranked
+	 * for a certain award, the value at position is a large number.
+	 * 
+	 * @return A 2D array that represents each nominee's ranking for each award
+	 */
+	private int[][] generateRankingMatrix(Award[] awards, ArrayList<String> noms) {
+		int numNoms = noms.size();
+		int[][] result = new int[numNoms][numNoms];
+		
+		final int HIGH = numNoms + 100; // "Ranking" for nominees not ranked for an award
+		
+		// Fill in all spots in the result for which there are rankings
+		for (Award award : awards) {
+			int awardIndex = award.getIndex();
+			for (String nom : award.getNoms()) {
+				int nomIndex = noms.indexOf(nom);
+				result[awardIndex][nomIndex] = nomIndex + 1;
+			}
+		}
+		
+		// Put HIGH in for all nominees not ranked for an award
+		for (int row = 0; row < numNoms; row++) {
+			for (int col = 0; col < numNoms; col++) {
+				if (result[row][col] == 0) result[row][col] = HIGH;
+			}
+		}
+		return result;
 	}
 }
