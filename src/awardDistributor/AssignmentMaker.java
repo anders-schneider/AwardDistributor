a872c7b8e9424910a8c42ec2549935c8ee067c0a
anders-schneider <aschn@seas.upenn.edu>
2015-05-09 17:44:11 -0400

    Initial code for Ford-Fulkerson Algorithm written; initial tests written, too

--- src/awardDistributor/AssignmentMaker.java
+++ src/awardDistributor/AssignmentMaker.java
@@ -26,7 +26,124 @@ public class AssignmentMaker {
 	 * @return HashMap with the key Award index and the value nominee index
 	 */
 	public static HashMap<Integer, Integer> findOptimalAssignments(int[][] rankingMatrix) {
+		boolean isPerfectMatching = false;
+		
+		while(!isPerfectMatching) {
+			
+			rowSubtractMin(rankingMatrix); // subtract minimum from each row
+			colSubtractMin(rankingMatrix); // subtract minimum from each column
+			
+			int[][] flowNetwork = findMaximumMatching(rankingMatrix);
+		}
+		
 		return null;
 	}
 	
+	/**
+	 * Subtracts the minimum element in the row from every element in the row.
+	 * 
+	 * @param matrix A 2D array of ints
+	 */
+	static void rowSubtractMin(int[][] matrix) {
+		for (int[] row : matrix) {
+			int min = findMin(row);
+			for (int col = 0; col < row.length; col++) row[col] -= min;
+		}
+	}
+	
+	/**
+	 * Subtracts the minimum element in the column from every element in
+	 * the column.
+	 * 
+	 * @param matrix A 2D array of ints
+	 */
+	static void colSubtractMin(int[][] matrix) {
+		for (int col = 0; col < matrix[0].length; col++) {
+			int min = findMin(matrix, col);
+			for (int row = 0; row < matrix.length; row++) matrix[row][col] -= min;
+		}
+	}
+	
+	/**
+	 * Returns the flow network that is generated when we use a variation
+	 * of the Ford-Fulkerson Max-Flow algorithm to find the maximum
+	 * matching of awards and nominees. In developing this code, I used 
+	 * <a href="https://www.youtube.com/watch?v=hmIrJCGPPG4">this Youtube tutorial</a>
+	 * in order to understand and adapt the algorithm for this project.
+	 * 
+	 * @param matrix Ranking matrix of awards and nominees
+	 * @return The flow network generated
+	 */
+	static int[][] findMaximumMatching(int[][] rankingMatrix) {
+		int numNoms = rankingMatrix.length;
+		
+		int[][] flowNetwork = new int[2 * numNoms + 2][2 * numNoms + 2];
+		int[][] adjMatrix = generateAdjacencyMatrix(rankingMatrix);
+		int[][] residualGraph = adjMatrix.clone();
+		
+		return null;
+	}
+	
+	/**
+	 * Returns an adjacency matrix for a directed graph that consists of a 
+	 * source vertex, a column of award vertices, a column of nominee
+	 * vertices, and a sink vertex. The source vertex has an edge to every
+	 * award vertex, and the sink vertex has an incoming edge from every
+	 * nominee vertex. The input ranking matrix determine the edges in the
+	 * middle; if there is a zero for the ith award and jth nominee, then
+	 * the ith award vertex has an edge to the jth nominee vertex.
+	 * 
+	 * @param rankingMatrix An 2D array of award-nominee rankings
+	 * @return A 2D array representing an adjacency matrix
+	 */
+	static int[][] generateAdjacencyMatrix(int[][] rankingMatrix) {
+		int numNoms = rankingMatrix.length;
+		
+		int[][] adjMatrix = new int[2 * numNoms + 2][2 * numNoms + 2];
+		
+		// Connect source vertex to all award vertices
+		for (int i = 1; i < numNoms + 1; i++) adjMatrix[0][i] = 1;
+		
+		// Connect all nominee vertices to sink vertex
+		int sinkIndex = 2 * numNoms + 1;
+		for (int i = numNoms + 1; i < sinkIndex; i++) adjMatrix[i][sinkIndex] = 1;
+		
+		// Connect award vertices to nominee vertices where the ranking matrix is 0
+		for (int a = 0; a < numNoms; a++) {
+			for (int n = 0; n < numNoms; n++) {
+				if (rankingMatrix[a][n] == 0) adjMatrix[a + 1][n + 1 + numNoms] = 1;
+			}
+		}
+		
+		return adjMatrix;
+	}
+	
+	/**
+	 * Returns the minimum element in the input row.
+	 * 
+	 * @param row An array of ints
+	 * @return The minimum of the array
+	 */
+	static int findMin(int[] row) {
+		int min = Integer.MAX_VALUE;
+		for (int elem : row) {
+			min = Math.min(min, elem);
+		}
+		return min;
+	}
+	
+	/**
+	 * Returns the minimum element in the specified column of the input matrix
+	 * 
+	 * @param matrix A 2D array of ints
+	 * @param col The index of the specified column
+	 * @return The minimum of the column
+	 */
+	static int findMin(int[][] matrix, int col) {
+		int min = Integer.MAX_VALUE;
+		for (int row = 0; row < matrix.length; row++) {
+			min = Math.min(min, matrix[row][col]);
+		}
+		return min;
+	}
 }
---
+++ src/awardDistributor/AssignmentMakerTest.java
@@ -0,0 +1,101 @@
+package awardDistributor;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AssignmentMakerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testFindOptimalAssignments() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRowSubtractMin() {
+		int[][] matrix = {{1,  2, 3},
+				  		  {5,  4, 3},
+				  		  {7, -3, 3}};
+
+		int[][] expected = {{0,  1, 2},
+				  			{2,  1, 0},
+				  			{10, 0, 6}};
+		
+		AssignmentMaker.rowSubtractMin(matrix);
+		assertArrayEquals(matrix, expected);
+	}
+
+	@Test
+	public void testColSubtractMin() {
+		int[][] matrix = {{1,  2, 3},
+				  		  {5,  4, 3},
+				  		  {7, -3, 3}};
+
+		int[][] expected = {{0, 5, 0},
+				  			{4, 7, 0},
+				  			{6, 0, 0}};
+		
+		AssignmentMaker.colSubtractMin(matrix);
+		assertArrayEquals(matrix, expected);
+	}
+
+	@Test
+	public void testFindMinRow() {
+		int[] arr1 = {2, 4, 1, 9, 13, 5};
+		int[] arr2 = {3, 3, 3};
+		int[] arr3 = {4, -2, 7, 23};
+		
+		assertEquals(1, AssignmentMaker.findMin(arr1));
+		assertEquals(3, AssignmentMaker.findMin(arr2));
+		assertEquals(-2, AssignmentMaker.findMin(arr3));
+	}
+
+	@Test
+	public void testFindMinColumn() {
+		int[][] matrix = {{1,  2, 3},
+						  {5,  4, 3},
+						  {7, -3, 3}};
+		
+		assertEquals(1, AssignmentMaker.findMin(matrix, 0));
+		assertEquals(-3, AssignmentMaker.findMin(matrix, 1));
+		assertEquals(3, AssignmentMaker.findMin(matrix, 2));
+	}
+	
+	@Test
+	public void testGenerateAdjacencyMatrix() {
+		int[][] rankingMatrix1 = {{0, 5, 0},
+	  							  {4, 7, 0},
+	  							  {6, 0, 0}};
+		
+		int[][] expected1 = {{0, 1, 1, 1, 0, 0, 0, 0},
+							 {0, 0, 0, 0, 1, 0, 1, 0},
+							 {0, 0, 0, 0, 0, 0, 1, 0},
+							 {0, 0, 0, 0, 0, 1, 1, 0},
+							 {0, 0, 0, 0, 0, 0, 0, 1},
+							 {0, 0, 0, 0, 0, 0, 0, 1},
+							 {0, 0, 0, 0, 0, 0, 0, 1},
+							 {0, 0, 0, 0, 0, 0, 0, 0}};
+		
+		assertArrayEquals(AssignmentMaker.generateAdjacencyMatrix(rankingMatrix1), expected1);
+		
+		int[][] rankingMatrix2 = {{0, 0,  0},
+								  {0, 7,  0},
+								  {6, 0, -2}};
+		
+		int[][] expected2 = {{0, 1, 1, 1, 0, 0, 0, 0},
+							 {0, 0, 0, 0, 1, 1, 1, 0},
+							 {0, 0, 0, 0, 1, 0, 1, 0},
+							 {0, 0, 0, 0, 0, 1, 0, 0},
+							 {0, 0, 0, 0, 0, 0, 0, 1},
+							 {0, 0, 0, 0, 0, 0, 0, 1},
+							 {0, 0, 0, 0, 0, 0, 0, 1},
+							 {0, 0, 0, 0, 0, 0, 0, 0}};
+		
+		assertArrayEquals(AssignmentMaker.generateAdjacencyMatrix(rankingMatrix2), expected2);
+	}
+}
